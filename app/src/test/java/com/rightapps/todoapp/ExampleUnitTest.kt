8ed7f486eeff013c105f91cbccf26fc7fd3203ae
Yogesh <ciderjd@gmail.com>
2023-10-23 13:51:18 +0530

    Initial

---
+++ app/src/test/java/com/rightapps/todoapp/ExampleUnitTest.kt
@@ -0,0 +1,31 @@
+package com.rightapps.todoapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    fun addTwoNumber(x: Long, y: Long): Long {
+
+        val result = x + y
+        println("x=$x, y=$y, Sum=$result")
+        return result
+    }
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(14, addTwoNumber(9, 5))
+
+    }
+
+    @Test
+    fun addition_isIncorrect() {
+        assertNotEquals(4, addTwoNumber(9, 5))
+
+    }
+}
